7f3833020d386c3dcc815fb1949e22ea6f4623ec
Eren GÜN <erengun4@gmail.com>
2024-06-29 18:29:57 +0300

    refactor: Remove unused server logger function

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,9 +1,7 @@
 const logger = {
-	server: (message) => console.log(`SERVER: ${message}`),
 	info: (message) => console.log(`INFO: ${message}`),
 	error: (message) => console.error(`ERROR: ${message}`),
 	debug: (message) => console.debug(`DEBUG: ${message}`),
 }
-
 
 module.exports = logger
