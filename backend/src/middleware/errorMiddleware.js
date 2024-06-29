2855a1362d59fc7f0c62c3ccc0a5471ffbe24766
Eren GÜN <erengun4@gmail.com>
2024-06-29 17:11:18 +0300

    chore: Update errorMiddleware.js to improve error handling and logging

--- backend/src/middleware/errorMiddleware.js
+++ backend/src/middleware/errorMiddleware.js
@@ -1,11 +1,11 @@
 const logger = require("../utils/logger");
 
+/// Error handler middleware
 module.exports = (err, req, res, next) => {
     let _error = {
-        message: err.message ?? "Somethings went wrong",
-        status: err.status ?? 500
+        message: err.message  || "Something went wrong",
+        status: err.status || 500
     };
-
-    logger.error(err.message);
+    logger.error(`${_error.message}, Status: ${_error.status}`)
     res.status(_error.status).json(_error);
 }
