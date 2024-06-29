f09197783f27661321528f61d57bc933528e6a0e
Eren GÜN <erengun4@gmail.com>
2024-06-29 16:14:55 +0300

    chore: Update authMiddleware.js to handle token validation and user retrieval from the database

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,17 +1,32 @@
-exports.verifyToken = async (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const user = await new Promise((res, rej) => {
-        cognitoExpress.validate(token, (err, response) => {
-            if (err) {
-                return rej({
-                    message: "Invalid token",
-                    status: 401
-                })
+
+const cognitoExpress = require('cognito-express')
+const client = require('../db-client');
+
+module.exports = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization')?.replace('Bearer ', '') ?? '';
+        const user = await new Promise((res, rej) => {
+            cognitoExpress.validate(token, (err, response) => {
+                if (err) {
+                    return rej({
+                        message: "Invalid token",
+                        status: 401
+                    })
+                }
+                res(response);
+            });
+        })
+        const dbUser = await client.user.findFirst({
+            where: {
+                email: user.email
             }
-            res(response);
-        });
-    })
-    // TODO: Fetch user from db
-    req.user = user;
-    next();
+        })
+    
+        req.user = dbUser;
+        next();
+      } catch (error) {
+        console.error("I AM HERE");
+        next(error);
+      }
+    
 };
