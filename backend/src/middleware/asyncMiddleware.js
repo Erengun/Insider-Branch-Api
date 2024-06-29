89782bbf10f1f5bb9c9d0ca59d16b7418720f9b1
Eren GÜN <erengun4@gmail.com>
2024-06-29 17:11:39 +0300

    chore: Wrap asynchronous middleware function with error handling in asyncMiddleware.js

--- backend/src/middleware/asyncMiddleware.js
+++ backend/src/middleware/asyncMiddleware.js
@@ -1,3 +1,9 @@
+/**
+ * Wraps an asynchronous middleware function with error handling.
+ *
+ * @param {function} fn - The asynchronous middleware function to be wrapped.
+ * @returns {function} - The wrapped middleware function.
+ */
 module.exports = fn => (req, res, next) =>
     Promise
       .resolve(fn(req, res, next))
