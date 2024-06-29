5044e122343fb3acf974e02389bc705b284f6810
Eren GÜN <erengun4@gmail.com>
2024-06-29 16:15:12 +0300

    chore: Update userRoutes.js to use asyncMiddleware for handling asynchronous operations in the login and register routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { login, register } = require('../controllers/userController');
-
+const asyncMiddleware = require('../middleware/asyncMiddleware');
 
 const router = express.Router();
 
 // Authentication route
-router.post('/login', login);
-router.post('/register', register);
+router.post('/login', asyncMiddleware(login));
+router.post('/register', asyncMiddleware(register));
 
 module.exports = router;
