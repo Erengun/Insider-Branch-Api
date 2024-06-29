b6210d8f92bfdc1541802305c6c8e7b18d294f80
Eren GÜN <erengun4@gmail.com>
2024-06-29 18:08:46 +0300

    chore: Refactor login page component structure

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -1,17 +1,15 @@
 import React from "react";
+import LoginComp from "./loginComp/loginComp";
+import RegisterComp from "./RegisterComp/registerComp";
 
 const Login: React.FC = () => {
-	const [emailFocus , setEmailFocus] = React.useState<boolean>(false);
+
 	return (
 		<>
-			<div className="w-full h-[100svh] bg-[#222222] flex justify-center items-center">
-				<div className="w-full max-w-lg h-96 border border-white rounded-md flex flex-col justify-around items-center">
-					<div className="w-11/12 relative">
-						<label htmlFor="" className="absolute top-0 left-0 -translate-y-1/2 text-white">E-Mail</label>
-						<input onFocus={() => {setEmailFocus(true)}} type="text" name="" id="" className="w-full h-16 bg-transparent border-b border-white focus:outline-none text-white pl-4" />
-					</div>
-					<input type="text" name="" id="" className="w-11/12 h-16 bg-transparent border-b border-white focus:outline-none text-white pl-4" />
-					<button>LOGIN</button>
+			<div className="w-full h-[100svh] bg-[#222222] flex flex-col gap-y-4 justify-center items-center overflow-hidden">
+				<div className="flex w-full max-w-lg h-96 relative">
+					{/* LOGIN */}
+					<LoginComp/>
 				</div>
 			</div>
 		</>
