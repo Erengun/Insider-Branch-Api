d9a7aec62551b1427e588dd03e6fa2711e8d6a93
Eren GÜN <erengun4@gmail.com>
2024-06-29 17:39:46 +0300

    chore: Add CRUD operations for branches in branchController.js

--- backend/src/controllers/branchController.js
+++ backend/src/controllers/branchController.js
@@ -1,11 +1,56 @@
 const client = require('../db-client');
 const logger = require("../utils/logger");
 
-async function list(req, res) {
+async function listBranches(req, res) {
     const branches = await client.branch.findMany({});
     res.status(200).json(branches);
 }
 
+async function addBranch(req, res) {
+    const { name, full_address, latitude, longitude, phone } = req.body;
+    const branch = await client.branch.create({
+        data: {
+            name,
+            full_address,
+            latitude,
+            longitude,
+            phone
+        }
+    });
+    res.status(201).json(branch);
+}
+
+async function editBranchDetails(req, res) {
+    const { id } = req.params;
+    const { name, full_address, latitude, longitude, phone } = req.body;
+    const branch = await client.branch.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: {
+            name,
+            full_address,
+            latitude,
+            longitude,
+            phone
+        }
+    });
+    res.status(200).json(branch);
+}
+
+async function removeBranch(req, res) {
+    const { id } = req.params;
+    const branch = await client.branch.delete({
+        where: {
+            id: parseInt(id)
+        }
+    });
+    res.status(200).json(branch);
+}
+
 module.exports = {
-    list
+    listBranches,
+    addBranch,
+    editBranchDetails,
+    removeBranch
 }
