b8870cc9d893567a42ab59b1bc4aad0fafd08ca1
Eren GÜN <erengun4@gmail.com>
2024-06-29 17:39:53 +0300

    chore: Update branchRoutes.js to add CRUD operations for branches

--- backend/src/routes/branchRoutes.js
+++ backend/src/routes/branchRoutes.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const { list } = require('../controllers/branchController');
+const { listBranches, editBranchDetails, removeBranch, addBranch } = require('../controllers/branchController');
 const authMiddleware = require('../middleware/authMiddleware');
 const asyncMiddleware = require('../middleware/asyncMiddleware');
 
 const router = express.Router();
 
 // Guard route
-router.use(authMiddleware);
+//router.use(authMiddleware);
 
-// List all branches
-router.get('/', asyncMiddleware(list));
+// List all branches (Everyone)
+router.get('/', asyncMiddleware(listBranches));
+
+// Add new branch
+router.post('/', asyncMiddleware(addBranch));
+
+// Update branch details
+router.put('/:id', asyncMiddleware(editBranchDetails));
+
+// Delete branch
+router.delete('/:id', asyncMiddleware(removeBranch));
 
 // View branch details
-// router.get('/:id', viewBranchDetails);
+//router.get('/:id', asyncMiddleware(viewBranchDetails));
 
 // Edit branch details (Owners only)
-// router.put('/:id', editBranchDetails);
+//router.put('/:id', asyncMiddleware(editBranchDetails));
 
 module.exports = router;
