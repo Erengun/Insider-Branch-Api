239c6db65dcd3b8eeaf2b21a844055161ec5803a
Eren GÜN <erengun4@gmail.com>
2024-06-30 15:33:09 +0300

    refactor: Add axios dependency and update npm dependencies for redux and react-redux

--- frontend/src/pages/dataPage/dataPage.tsx
+++ frontend/src/pages/dataPage/dataPage.tsx
@@ -1,35 +1,57 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { changeData } from '../../redux/dataSlice';
 import { RootState } from '../../redux/store';
 
 type BranchData = {
-  branch_id: string;
+  id: string;
   name: string;
   full_address: string;
   latitude: number;
-  longtitude: number;
+  longitude: number;
   phone: string;
 };
 
 const DataPage: React.FC = () => {
   const dataSlice = useSelector((state: RootState) => state.dataSlice);
   const dispatch = useDispatch();
+  const [login, setLogin] = React.useState<boolean>(false);
+  const [data, setData] = useState<BranchData[]>(dataSlice);
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.table(dataSlice);
+    setData(dataSlice);
+    setLogin(true);
   }, [dataSlice]);
 
-  const [data, setData] = useState<BranchData[]>(dataSlice);
-  const [editIndex, setEditIndex] = useState<number | null>(null);
 
   const handleEdit = (index: number) => {
     setEditIndex(index);
   };
 
-  const handleSave = () => {
-    dispatch(changeData(data));
-    setEditIndex(null);
+  const handleSave = async () => {
+    const branch = data[editIndex as number]
+    console.log(branch, " ", editIndex)
+    try {
+      await axios.put(`${import.meta.env.VITE_BASE_URL}/branches/${branch.id}`, branch, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('access-token')}`
+        },
+        validateStatus(status) {
+          return status == 200
+        },
+      })
+      dispatch(changeData(data));
+    } catch (e) {
+      setData(dataSlice);
+      console.error(`Error While Save Data => ${e}`)
+    } finally {
+      setEditIndex(null);
+    }
   };
 
   const handleCancel = () => {
@@ -47,46 +69,85 @@ const DataPage: React.FC = () => {
     console.table(data);
   }, [data]);
 
+  React.useEffect(() => {
+
+    if (!localStorage.getItem('access-token')){
+      navigate('/');
+    }
+
+    const fn = async () => {
+      await axios.get(`${import.meta.env.VITE_BASE_URL}/branches`, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('access-token')}`
+        }
+      }).then(response => {
+        dispatch(changeData(response.data));
+      })
+    }
+
+    try {
+      fn();
+    } catch (e){
+      console.error(`Error While Page Opening => ${e}`);
+      navigate('/');
+    }
+  }, [])
+
   return (
-    <div className="w-full h-[100svh] bg-[#222222] flex justify-center items-center">
-      <div className="w-11/12 h-5/6 border border-white rounded-md overflow-scroll relative">
-        <div className="grid grid-cols-[5%_10%_30%_10%_10%_20%_15%] w-full h-16 sticky top-0 left-0 rounded-t-md bg-[#222222] border-b border-white">
-          {['ID', 'Name', 'Address', 'Latitude', 'Longtitude', 'Phone', 'Edit'].map((header, index) => (
-            <div key={index} className={`flex justify-center items-center uppercase text-white ${index % 2 === 0 ? 'bg-[#444444]' : ''}`}>
-              {header}
+    <div className="w-full h-[100svh] bg-[#222222] flex justify-center items-center relative">
+      <button
+              onClick={() => {
+                localStorage.removeItem('access-token')
+                localStorage.removeItem('role');
+                navigate('/');
+              }}
+              className='border border-white px-8 py-2 rounded-md absolute top-0 right-0 text-white'> LOGOUT</button>
+      {
+        (login) &&
+        <div className="w-11/12 h-5/6 border border-white rounded-md overflow-scroll relative">
+          <div className={`grid ${localStorage.getItem('role') == 'OWNER' ? 'grid-cols-[5%_10%_30%_10%_10%_20%_15%]' : 'grid-cols-[5%_10%_30%_10%_10%_auto]'} w-full h-16 sticky top-0 left-0 rounded-t-md bg-[#222222] border-b border-white`}>
+            {['ID', 'Name', 'Address', 'Latitude', 'longitude', 'Phone', localStorage.getItem('role') == 'OWNER' ? 'Edit' : undefined].map((header, index) => (
+              header ?
+                <div key={index} className={`flex justify-center items-center uppercase text-white ${index % 2 === 0 ? 'bg-[#444444]' : ''}`}>
+                  {header}
+                </div> : undefined
+            ))}
+          </div>
+          {data.map((item, index) => (
+            <div key={index} className={`grid ${localStorage.getItem('role') == 'OWNER' ? 'grid-cols-[5%_10%_30%_10%_10%_20%_15%]' : 'grid-cols-[5%_10%_30%_10%_10%_auto]'} w-full h-16 ${index % 2 === 0 ? 'bg-[#444444]' : ''}`}>
+              <div className="flex justify-center items-center text-white">{index}</div>
+              {['name', 'full_address', 'latitude', 'longitude', 'phone'].map((field, i) => (
+                <div key={i} className="flex justify-center items-center text-white">
+                  {editIndex === index ? (
+                    <input
+                      type={field === 'latitude' || field === 'longitude' ? 'number' : 'text'}
+                      value={item[field as keyof BranchData]}
+                      onChange={(e) => handleChange(index, field as keyof BranchData, field === 'latitude' || field === 'longitude' ? parseFloat(e.target.value) : e.target.value)}
+                      className="w-full bg-transparent text-white p-3 rounded-lg border border-white/30 outline-none focus:border-white/60 text-center"
+                    />
+                  ) : (
+                    item[field as keyof BranchData]
+                  )}
+                </div>
+              ))}
+              {
+                localStorage.getItem('role') == 'OWNER' ?
+                  <div className="flex justify-center items-center text-white gap-x-2">
+                    {editIndex === index ? (
+                      <div className="flex flex-col gap-2">
+                        <button onClick={handleSave} className="text-green-500">Save</button>
+                        <button onClick={handleCancel} className="text-red-500">Cancel</button>
+                      </div>
+                    ) : (
+                      <button onClick={() => handleEdit(index)} className="text-blue-500">Edit</button>
+                    )}
+                  </div>
+                  : <></>
+              }
             </div>
           ))}
         </div>
-        {data.map((item, index) => (
-          <div key={index} className={`grid grid-cols-[5%_10%_30%_10%_10%_20%_15%] w-full h-16 ${index % 2 === 0 ? 'bg-[#444444]' : ''}`}>
-            <div className="flex justify-center items-center text-white">{index}</div>
-            {['name', 'full_address', 'latitude', 'longtitude', 'phone'].map((field, i) => (
-              <div key={i} className="flex justify-center items-center text-white">
-                {editIndex === index ? (
-                  <input
-                    type={field === 'latitude' || field === 'longtitude' ? 'number' : 'text'}
-                    value={item[field as keyof BranchData]}
-                    onChange={(e) => handleChange(index, field as keyof BranchData, field === 'latitude' || field === 'longtitude' ? parseFloat(e.target.value) : e.target.value)}
-                    className="w-full bg-transparent text-white p-3 rounded-lg border border-white/30 outline-none focus:border-white/60"
-                  />
-                ) : (
-                  item[field as keyof BranchData]
-                )}
-              </div>
-            ))}
-            <div className="flex justify-center items-center text-white gap-x-2">
-              {editIndex === index ? (
-                <div className="flex flex-col gap-2">
-                  <button onClick={handleSave} className="text-green-500">Save</button>
-                  <button onClick={handleCancel} className="text-red-500">Cancel</button>
-                </div>
-              ) : (
-                <button onClick={() => handleEdit(index)} className="text-blue-500">Edit</button>
-              )}
-            </div>
-          </div>
-        ))}
-      </div>
+      }
     </div>
   );
 };
--- frontend/src/pages/login/loginComp/loginComp.tsx
+++ frontend/src/pages/login/loginComp/loginComp.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,10 +7,33 @@ const LoginComp: React.FC = () => {
   // Set Change Animation State For Email Input
   const [emailFocus, setEmailFocus] = React.useState<boolean>(false);
   const emailInputRef = React.useRef<HTMLInputElement | null>(null);
+  const [emailInputData, setEmailInputData] = React.useState<string>("");
 
   // Set Change Animation State For Password Input
   const [passwordFocus, setPasswordFocus] = React.useState<boolean>(false);
   const passwordInputRef = React.useRef<HTMLInputElement | null>(null);
+  const [passwordInputData, setPasswordInputData] = React.useState<string>("");
+
+  const [showPassword, setShowPassword] = React.useState<boolean>(false);
+
+  const handleLogin = async () => {
+    console.log(import.meta.env.VITE_BASE_URL);
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`, {
+        email: emailInputData,
+        password: passwordInputData
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      localStorage.setItem('access-token' , response.data.accessToken);
+      localStorage.setItem('role' , response.data.role);
+      navigate('/home');
+    }catch (e){
+      console.error(`Error While Login => ${e}`)
+    }
+  }
 
   const navigate = useNavigate();
   return (
@@ -24,13 +48,14 @@ const LoginComp: React.FC = () => {
           <input ref={emailInputRef}
             onFocus={() => { setEmailFocus(true) }}
             onBlur={(e) => { setEmailFocus(e.target.value.length > 0) }}
-            type="mail"
+            onChange={(e) => { setEmailInputData(e.target.value) }}
+            type="email"
             name=""
             id=""
             className={`w-full h-16 bg-transparent border-b border-white focus:outline-none text-white pl-4`} />
         </div>
 
-        <div className="w-11/12 relative">
+        <div className="w-11/12 relative flex">
           <label onClick={(() => {
             if (passwordInputRef && passwordInputRef.current) {
               passwordInputRef.current.focus();
@@ -39,14 +64,20 @@ const LoginComp: React.FC = () => {
           <input ref={passwordInputRef}
             onFocus={() => { setPasswordFocus(true) }}
             onBlur={(e) => { setPasswordFocus(e.target.value.length > 0) }}
-            type="password"
+            onChange={(e) => { setPasswordInputData(e.target.value) }}
+            type={showPassword ? "text" : "password"}
             name=""
             id="" className={`w-full h-16 bg-transparent border-b border-white focus:outline-none text-white pl-4`} />
+          <button onClick={() => { setShowPassword(prev => !prev) }} className="text-white fill-white border-b border-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24"><path fill="white" d="M2 17h20v2H2zm1.15-4.05L4 11.47l.85 1.48l1.3-.75l-.85-1.48H7v-1.5H5.3l.85-1.47L4.85 7L4 8.47L3.15 7l-1.3.75l.85 1.47H1v1.5h1.7l-.85 1.48zm6.7-.75l1.3.75l.85-1.48l.85 1.48l1.3-.75l-.85-1.48H15v-1.5h-1.7l.85-1.47l-1.3-.75L12 8.47L11.15 7l-1.3.75l.85 1.47H9v1.5h1.7zM23 9.22h-1.7l.85-1.47l-1.3-.75L20 8.47L19.15 7l-1.3.75l.85 1.47H17v1.5h1.7l-.85 1.48l1.3.75l.85-1.48l.85 1.48l1.3-.75l-.85-1.48H23z" /></svg>
+          </button>
 
         </div>
 
-        <button onClick={() => {navigate('/home')}}
-                className="w-11/12 bg-white bg-white h-10 rounded-sm">LOGIN</button>
+        <button
+          disabled={!(emailInputData.length >= 1 && passwordInputData.length >= 1)}
+          onClick={handleLogin}
+          className={`w-11/12 bg-white bg-white h-10 rounded-sm ${!(emailInputData.length >= 1 && passwordInputData.length >= 1) ? ' opacity-50 cursor-not-allowed' : 'opacity-100 cursor-pointer'}`}>LOGIN</button>
       </div>
     </>
   )
