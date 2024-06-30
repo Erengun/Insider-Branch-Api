3ca9ee655e4ff06729fab29ee3d948efb3ef3a40
Eren GÜN <erengun4@gmail.com>
2024-06-30 14:13:20 +0300

    refactor: Improve permission middleware to handle unauthorized requests

--- backend/src/middleware/permissionMiddleware.js
+++ backend/src/middleware/permissionMiddleware.js
@@ -1,16 +1,22 @@
 const { Role } = require("@prisma/client");
+const logger = require("../utils/logger");
 
 module.exports = (req, res, next) => {
 	if (!req.user) {
-		res.status(403);
-		throw new Error('Not authorized');
+		throw {
+			message: "Not authorized",
+			status: 403
+		}
 	}
 	// Check for owner role and restrict access to certain routes
 	if (['POST', 'PUT'].includes(req.method)) {
-			if (req.user.role !== Role.OWNER) {
-				res.status(403).send('Not authorized. Only owners can perform this action.');
-				return; // Stop execution if the user is not an owner
+		if (req.user.role !== Role.OWNER) {
+			// Stop execution if the user is not an owner
+			throw {
+				message: 'Not authorized. Only owners can perform this action.',
+				status: 403
 			}
+		}
 	}
 	next();
 };
